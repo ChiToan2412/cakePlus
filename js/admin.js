f3aeda2ece69ec996a87e1243005d825b9762100
ChiToan <167092655+ChiToan2412@users.noreply.github.com>
2024-11-20 00:16:25 +0700

    update js

--- js/admin.js
+++ js/admin.js
@@ -6,18 +6,19 @@ const fetchAPI = async (url, option) =>{
 const loadProducts = async () => {
     let response = await fetch(root + "products");
     let products = await response.json();
-    console.log(products);
+
     document.getElementById("allProduct").innerHTML = products.map((item)=>{
+        let price = item.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })
         return `
             <tr>
-                <td>1</td>
-                <td><img src="https://via.placeholder.com/50" alt="product-image" class="img-thumbnail"></td>
-                <td>Bánh Chocolate</td>
-                <td>Bánh mềm thơm, vị chocolate đậm đà.</td>
-                <td>Bánh ngọt</td>
-                <td>100,000 VND</td>
+                <td>${item.id}</td>
+                <td><img src="../public/img/${item.image}" alt="product-image" class="img-thumbnail" width="75px" height="75px"></td>
+                <td>${item.name}</td>
+                <td>${item.description}</td>
+                <td>${item.category}</td>
+                <td>${price}</td>
                 <td>
-                <button class="btn btn-sm btn-warning">Sửa</button>
+                <button class="btn btn-sm btn-warning" onclick="handleButtonProduct(${item.id})">Sửa</button>
                 <button class="btn btn-sm btn-danger">Xóa</button>
                 </td>
             </tr>
@@ -40,7 +41,58 @@ const loadCategories = async () => {
         `;
     }).join("");
 }
-const startJs = () => {
+
+const handleButtonProduct = async (index = -1) => {
+    let button = document.getElementById("btn");
+    let res = await fetch(root + "products");
+    let products = await res.json();
+    if(index != -1){  
+        document.getElementById("productId").value = products[index-1].id;
+        document.getElementById("productName").value = products[index-1].name;
+        document.getElementById("productPrice").value = products[index-1].price;
+        document.getElementById("productCategory").value = products[index-1].category;
+        document.getElementById("productDescription").value = products[index-1].description;
+        document.getElementById("img").src = "../public/img/"+products[index-1].image;
+        button.value = "Sửa";
+    }
+    button.onclick = () => {
+        let id = document.getElementById("productId").value;
+        let name = document.getElementById("productName").value;
+        let price = document.getElementById("productPrice").value;
+        let category = document.getElementById("productCategory").value;
+        let description = document.getElementById("productDescription").value;
+        let img = "";
+        let addressImg = document.getElementById("productImage").value.split("\\");
+        if(addressImg){
+            img = addressImg[addressImg.length-1];
+        }
+        let newProduct = {
+            "id": id,
+            "name": name,
+            "price": price,
+            "category": category,
+            "description": description,
+            "image": img
+        } 
+        if(button.value == "AddProduct"){
+            let url = root + "products";
+            let options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newProduct)
+            }
+            fetchAPI(url, options);
+        }else{
+            
+        }
+    }
+}
+const startJsProducts = () => {
     loadProducts();
+    handleButtonProduct();
+}
+const startJsCategories = () => {
     loadCategories();
 }
